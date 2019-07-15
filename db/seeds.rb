75baa9545fa6464506d3f7665d738247f3bf1cca
Steven Torrence <storrence88@gmail.com>
2019-07-15 08:56:00 -0500

    Add Faker gem and create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "Blog post No. #{blog + 1}",
+    body: Faker::Lorem.paragraph(2, false, 8)
+  )
+end
+
+puts '10 Blogs created!'
+
+5.times do |skill|
+  Skill.create!(
+    title: "Skill No. #{skill + 1}",
+    percent: 15
+  )
+end
+
+puts '10 Skills created!'
+
+9.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio No. #{portfolio + 1}",
+    subtitle: Faker::Lorem.sentences(1, true),
+    body: Faker::Lorem.paragraph(2, false, 5),
+    thumb_image: Faker::LoremPixel.image("350x200"),
+    main_image: Faker::LoremPixel.image("600x400")
+  )
+end
+
+puts '10 Portfolio Items created!'
