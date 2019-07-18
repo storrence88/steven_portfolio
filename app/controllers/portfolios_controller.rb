ea3bf43b6bd5e0293e17618cb829b61e527e7d46
Steven Torrence <storrence88@gmail.com>
2019-07-17 20:56:24 -0500

    Use attr_reader to replace instance variable

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,20 @@
 class PortfoliosController < ApplicationController
+  helper_method :portfolio_items
 
   # Renders the index view
   def index
     @portfolio_items = Portfolio.all
   end
+
+  # Renders the new view
+  def new
+  end
+
+  # Creates a new Portfolio record
+  def create
+  end
+
+  private
+
+  attr_reader :portfolio_items
 end
