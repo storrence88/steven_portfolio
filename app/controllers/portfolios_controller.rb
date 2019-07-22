75c057b6695d584abbe312ee0e16728f4d84fd73
Steven Torrence <storrence88@gmail.com>
2019-07-21 22:08:56 -0500

    Create the create action for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,11 +11,28 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   # Creates a new Portfolio record
   def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item, notice: 'Portfolio Item was successfully created!' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   private
 
   attr_reader :portfolio_items, :portfolio_item
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
